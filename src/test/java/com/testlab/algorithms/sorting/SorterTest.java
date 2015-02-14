6f7fd916ad415917c6fab1233fe3a982dd03dd79
Paulo Schneider <paulo.schneider@gmail.com>
2015-02-14 14:38:23 +0000

    Sorting with duplicates

--- src/test/java/com/testlab/algorithms/sorting/SorterTest.java
+++ src/test/java/com/testlab/algorithms/sorting/SorterTest.java
@@ -3,7 +3,6 @@ package com.testlab.algorithms.sorting;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 
 public class SorterTest {
@@ -15,5 +14,14 @@ public class SorterTest {
         Sorter.inplaceSort(elements);
 
         assertThat(elements, equalTo(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+    }
+
+    @Test
+    public void sortListWithDuplicates() throws Exception {
+        int[] elements = {4, 5, 1, 1, 5, 7, 3, 2, 3, 6};
+
+        Sorter.inplaceSort(elements);
+
+        assertThat(elements, equalTo(new int[] {1, 1, 2, 3, 3, 4, 5, 5, 6, 7}));
     }
 }
