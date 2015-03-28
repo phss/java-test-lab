252206e448fd26fcc3d053bdc25b69c3a9d09c49
Paulo Schneider <paulo.schneider@gmail.com>
2015-03-28 09:27:14 +0000

    Extracting methods

--- src/main/java/com/testlab/algorithms/datastructures/CircularListBasedQueue.java
+++ src/main/java/com/testlab/algorithms/datastructures/CircularListBasedQueue.java
@@ -5,22 +5,18 @@ import java.util.ArrayList;
 public class CircularListBasedQueue<T> implements Queue<T> {
 
     private final ArrayList<T> array = new ArrayList<>();
-    private final int maxSize;
     private int headIndex = -1;
     private int tailIndex = -1;
     private int counts = 0;
 
     public CircularListBasedQueue(int maxSize) {
-        this.maxSize = maxSize;
-        for (int i = 0; i < maxSize; i++) {
-            array.add(null);
-        }
+        initializeArray(maxSize);
     }
 
     @Override
     public void enqueue(T element) {
         counts++;
-        tailIndex = (tailIndex + 1) % maxSize;
+        incrementTail();
         array.set(tailIndex, element);
         if (headIndex < 0) {
             headIndex = 0;
@@ -34,14 +30,33 @@ public class CircularListBasedQueue<T> implements Queue<T> {
         }
         counts--;
 
-        T headElement = array.get(headIndex);
-        array.set(headIndex, null);
-        headIndex = (headIndex + 1) % maxSize;
+        T headElement = getAndRemoveFromHead();
+        incrementHead();
         return headElement;
     }
 
     @Override
     public boolean isEmpty() {
         return counts == 0;
+    }
+
+    private void initializeArray(int maxSize) {
+        for (int i = 0; i < maxSize; i++) {
+            array.add(i, null);
+        }
+    }
+
+    private void incrementTail() {
+        tailIndex = (tailIndex + 1) % array.size();
+    }
+
+    private void incrementHead() {
+        headIndex = (headIndex + 1) % array.size();
+    }
+
+    private T getAndRemoveFromHead() {
+        T headElement = array.get(headIndex);
+        array.set(headIndex, null);
+        return headElement;
     }
 }
